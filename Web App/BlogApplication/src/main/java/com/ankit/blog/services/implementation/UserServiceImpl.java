880a7638460bf79c09092573218fef7a7333c14d
Ankit Sharma <sharmankit041@gmail.com>
2024-06-26 19:23:17 +0530

    updated

--- Web App/BlogApplication/src/main/java/com/ankit/blog/services/implementation/UserServiceImpl.java
+++ Web App/BlogApplication/src/main/java/com/ankit/blog/services/implementation/UserServiceImpl.java
@@ -118,53 +118,9 @@ public class UserServiceImpl implements UserService {
 		HSSFWorkbook workbook = new HSSFWorkbook();
 		HSSFSheet sheet = workbook.createSheet("User Info");
 
-<<<<<<< HEAD
-	    HSSFFont calibriFont = workbook.createFont();
-	    calibriFont.setFontName("Calibri");
-
-	 
-	    HSSFCellStyle headerStyle = workbook.createCellStyle();
-	    headerStyle.setFillForegroundColor(HSSFColor.HSSFColorPredefined.LIGHT_YELLOW.getIndex());
-	    headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-	    headerStyle.setBorderTop(BorderStyle.THIN);
-	    headerStyle.setBorderBottom(BorderStyle.THIN);
-	    headerStyle.setBorderLeft(BorderStyle.THIN);
-	    headerStyle.setBorderRight(BorderStyle.THIN);
-	    
-	    HSSFFont headerFont = workbook.createFont();
-	    headerFont.setBold(true);
-	    headerFont.setFontName("Calibri");
-	    headerStyle.setFont(headerFont);
-
-	   
-	    HSSFCellStyle evenRowStyle = workbook.createCellStyle();
-	    evenRowStyle.setFillForegroundColor(HSSFColor.HSSFColorPredefined.LIGHT_GREEN.getIndex());
-	    evenRowStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-	    evenRowStyle.setBorderTop(BorderStyle.THIN);
-	    evenRowStyle.setBorderBottom(BorderStyle.THIN);
-	    evenRowStyle.setBorderLeft(BorderStyle.THIN);
-	    evenRowStyle.setBorderRight(BorderStyle.THIN);
-	    evenRowStyle.setFont(calibriFont);
-
-	  
-	    HSSFCellStyle oddRowStyle = workbook.createCellStyle();
-	    oddRowStyle.setFillForegroundColor(HSSFColor.HSSFColorPredefined.LIGHT_TURQUOISE.getIndex());
-	    oddRowStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-	    oddRowStyle.setBorderTop(BorderStyle.THIN);
-	    oddRowStyle.setBorderBottom(BorderStyle.THIN);
-	    oddRowStyle.setBorderLeft(BorderStyle.THIN);
-	    oddRowStyle.setBorderRight(BorderStyle.THIN);
-	    oddRowStyle.setFont(calibriFont);
-
-	   
-	    HSSFRow headerRow = sheet.createRow(0);
-	    HSSFCell cell;
-=======
-		// Create a font for the header and data cells
 		HSSFFont calibriFont = workbook.createFont();
 		calibriFont.setFontName("Calibri");
 
-		// Create header style with bold Calibri font
 		HSSFCellStyle headerStyle = workbook.createCellStyle();
 		headerStyle.setFillForegroundColor(HSSFColor.HSSFColorPredefined.LIGHT_YELLOW.getIndex());
 		headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
@@ -178,7 +134,6 @@ public class UserServiceImpl implements UserService {
 		headerFont.setFontName("Calibri");
 		headerStyle.setFont(headerFont);
 
-		// Create data style for even rows with Calibri font
 		HSSFCellStyle evenRowStyle = workbook.createCellStyle();
 		evenRowStyle.setFillForegroundColor(HSSFColor.HSSFColorPredefined.LIGHT_GREEN.getIndex());
 		evenRowStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
@@ -188,7 +143,6 @@ public class UserServiceImpl implements UserService {
 		evenRowStyle.setBorderRight(BorderStyle.THIN);
 		evenRowStyle.setFont(calibriFont);
 
-		// Create data style for odd rows with Calibri font
 		HSSFCellStyle oddRowStyle = workbook.createCellStyle();
 		oddRowStyle.setFillForegroundColor(HSSFColor.HSSFColorPredefined.LIGHT_TURQUOISE.getIndex());
 		oddRowStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
@@ -197,75 +151,113 @@ public class UserServiceImpl implements UserService {
 		oddRowStyle.setBorderLeft(BorderStyle.THIN);
 		oddRowStyle.setBorderRight(BorderStyle.THIN);
 		oddRowStyle.setFont(calibriFont);
->>>>>>> 59ffd6630cafaf471594556cfcb3356245d6d959
 
-		// Create header row
 		HSSFRow headerRow = sheet.createRow(0);
 		HSSFCell cell;
 
-<<<<<<< HEAD
-	    
-	    int dataRowIndex = 1;
-	    int serialNumber = 1;
-	    for (User user : users) {
-	        HSSFRow dataRow = sheet.createRow(dataRowIndex);
-	        HSSFCellStyle rowStyle = (dataRowIndex % 2 == 0) ? evenRowStyle : oddRowStyle;
-=======
-		String[] headers = { "S.No", "USER_ID", "USER_NAME", "EMAIL_ID", "PASSWORD", "ABOUT_USER", "ROLE" };
-		for (int i = 0; i < headers.length; i++) {
-			cell = headerRow.createCell(i);
-			cell.setCellValue(headers[i]);
-			cell.setCellStyle(headerStyle);
-		}
->>>>>>> 59ffd6630cafaf471594556cfcb3356245d6d959
+		// Create a font for the header and data cells
+		HSSFFont calibriFont1 = workbook.createFont();
+		calibriFont1.setFontName("Calibri");
 
-		// Create data rows
+		// Create header style with bold Calibri font
+		HSSFCellStyle headerStyle1 = workbook.createCellStyle();
+		headerStyle1.setFillForegroundColor(HSSFColor.HSSFColorPredefined.LIGHT_YELLOW.getIndex());
+		headerStyle1.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+		headerStyle1.setBorderTop(BorderStyle.THIN);
+		headerStyle1.setBorderBottom(BorderStyle.THIN);
+		headerStyle1.setBorderLeft(BorderStyle.THIN);
+		headerStyle1.setBorderRight(BorderStyle.THIN);
+
+		HSSFFont headerFont1 = workbook.createFont();
+		headerFont1.setBold(true);
+		headerFont1.setFontName("Calibri");
+		headerStyle1.setFont(headerFont1);
+
+		// Create data style for even rows with Calibri font
+		HSSFCellStyle evenRowStyle1 = workbook.createCellStyle();
+		evenRowStyle1.setFillForegroundColor(HSSFColor.HSSFColorPredefined.LIGHT_GREEN.getIndex());
+		evenRowStyle1.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+		evenRowStyle1.setBorderTop(BorderStyle.THIN);
+		evenRowStyle1.setBorderBottom(BorderStyle.THIN);
+		evenRowStyle1.setBorderLeft(BorderStyle.THIN);
+		evenRowStyle1.setBorderRight(BorderStyle.THIN);
+		evenRowStyle1.setFont(calibriFont1);
+
+		// Create data style for odd rows with Calibri font
+		HSSFCellStyle oddRowStyle1 = workbook.createCellStyle();
+		oddRowStyle1.setFillForegroundColor(HSSFColor.HSSFColorPredefined.LIGHT_TURQUOISE.getIndex());
+		oddRowStyle1.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+		oddRowStyle1.setBorderTop(BorderStyle.THIN);
+		oddRowStyle1.setBorderBottom(BorderStyle.THIN);
+		oddRowStyle1.setBorderLeft(BorderStyle.THIN);
+		oddRowStyle1.setBorderRight(BorderStyle.THIN);
+		oddRowStyle1.setFont(calibriFont1);
+
+		// Create header row
+		HSSFRow headerRow1 = sheet.createRow(0);
+		HSSFCell cell1;
+
 		int dataRowIndex = 1;
 		int serialNumber = 1;
 		for (User user : users) {
 			HSSFRow dataRow = sheet.createRow(dataRowIndex);
-			HSSFCellStyle rowStyle = (dataRowIndex % 2 == 0) ? evenRowStyle : oddRowStyle;
+			HSSFCellStyle rowStyle = (dataRowIndex % 2 == 0) ? evenRowStyle1 : oddRowStyle1;
 
-			cell = dataRow.createCell(0);
-			cell.setCellValue(serialNumber);
-			cell.setCellStyle(rowStyle);
+			String[] headers = { "S.No", "USER_ID", "USER_NAME", "EMAIL_ID", "PASSWORD", "ABOUT_USER", "ROLE" };
+			for (int i = 0; i < headers.length; i++) {
+				cell1 = headerRow1.createCell(i);
+				cell1.setCellValue(headers[i]);
+				cell1.setCellStyle(headerStyle);
+			}
 
-			cell = dataRow.createCell(1);
-			cell.setCellValue(user.getId());
-			cell.setCellStyle(rowStyle);
+			// Create data rows
+			int dataRowIndex1 = 1;
+			int serialNumber1 = 1;
+			for (User user1 : users) {
+				HSSFRow dataRow1 = sheet.createRow(dataRowIndex1);
+				HSSFCellStyle rowStyle1 = (dataRowIndex1 % 2 == 0) ? evenRowStyle : oddRowStyle;
 
-			cell = dataRow.createCell(2);
-			cell.setCellValue(user.getName());
-			cell.setCellStyle(rowStyle);
+				cell1 = dataRow1.createCell(0);
+				cell1.setCellValue(serialNumber1);
+				cell1.setCellStyle(rowStyle1);
 
-			cell = dataRow.createCell(3);
-			cell.setCellValue(user.getEmail());
-			cell.setCellStyle(rowStyle);
+				cell1 = dataRow1.createCell(1);
+				cell1.setCellValue(user1.getId());
+				cell1.setCellStyle(rowStyle1);
 
-			cell = dataRow.createCell(4);
-			cell.setCellValue(user.getPassword());
-			cell.setCellStyle(rowStyle);
+				cell1 = dataRow1.createCell(2);
+				cell1.setCellValue(user1.getName());
+				cell1.setCellStyle(rowStyle1);
 
-			cell = dataRow.createCell(5);
-			cell.setCellValue(user.getAbout());
-			cell.setCellStyle(rowStyle);
+				cell1 = dataRow1.createCell(3);
+				cell1.setCellValue(user1.getEmail());
+				cell1.setCellStyle(rowStyle1);
 
-			cell = dataRow.createCell(6);
-			cell.setCellValue(user.getRole());
-			cell.setCellStyle(rowStyle);
+				cell1 = dataRow1.createCell(4);
+				cell1.setCellValue(user1.getPassword());
+				cell1.setCellStyle(rowStyle1);
 
-			dataRowIndex++;
-			serialNumber++;
+				cell1 = dataRow1.createCell(5);
+				cell1.setCellValue(user1.getAbout());
+				cell1.setCellStyle(rowStyle1);
+
+				cell1 = dataRow1.createCell(6);
+				cell1.setCellValue(user1.getRole());
+				cell1.setCellStyle(rowStyle1);
+
+				dataRowIndex1++;
+				serialNumber1++;
+			}
+
+			try {
+				ServletOutputStream ops = response.getOutputStream();
+				workbook.write(ops);
+				workbook.close();
+				ops.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 
-		try {
-			ServletOutputStream ops = response.getOutputStream();
-			workbook.write(ops);
-			workbook.close();
-			ops.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
-
 }
